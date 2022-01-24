631d0d8ff8ce2972ace10f84026d80f6a2c14c1a
ys <2850281406@qq.com>
2022-01-24 16:56:08 +0800

    ys

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,15 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-01-24 16:30:29
+ * @LastEditTime: 2022-01-24 16:56:00
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \blog\src\components\HelloWorld.vue
+-->
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
