7f80c4964ee8d46fa4bd6e3a1a74c3620232c701
ys <2850281406@qq.com>
2022-01-25 13:59:50 +0800

    ys

--- blog-deploy.sh
+++ blog-deploy.sh
@@ -1,4 +1,12 @@
 echo -e "---------登录镜像容器服务--------"
+###
+ # @Author: your name
+ # @Date: 2022-01-21 15:29:18
+ # @LastEditTime: 2022-01-25 13:59:02
+ # @LastEditors: your name
+ # @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ # @FilePath: \blog\blog-deploy.sh
+### 
 # 登录阿里云镜像容器服务
 docker login --username=healerrr registry.cn-hangzhou.aliyuncs.com --password=ys19940912ysxin2
 echo -e "---------停掉镜像--------"
